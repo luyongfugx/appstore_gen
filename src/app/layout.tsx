c7b58f1f4f50ee435e8253208ad8c127bf382b8d
waynelu <luyongfugx@163.com>
2024-09-19 21:11:53 +0800

    landing page keyword

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,12 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AppstoreScreenShots  ",
+  title:
+    "AppstoreScreenShots  | App Store Screenshot Generator | Free for iOS &amp; Android",
   description:
-    "appstore ,screenshots, appscreens , Screenshot Creator,Create stunning App Store and Google Play marketing assets ",
+    "Effortlessly design high-quality, designer-grade screenshots for iOS &amp; Android app stores with AppScreens. Save hours with our screenshot generator, supporting all sizes for App Store &amp; Google Play. Try it for free and elevate your app's presence today! Work with a single responsive design to generate app store-ready images for all iOS and Android devices, from phones and tablets to watches and laptops. Catering to both App Store Connect and Google Play, receive pixel-perfect JPEG and PNG files ready to upload. Our smart-export ensures perfect sizing across all devices and languages. Jump right in and simplify your app release today.",
+  keywords:
+    "appscreens,app screens,appscreens.com,app screenshot generator,screenshot maker,app screenshot maker,screenshot creator,app screenshot creator,create app screenshots,screenshot generator,app store image,google play image,ios screenshot,android screenshot,applaunchpad,shotbot,appure,mockup,mockup generator",
 };
 
 export default function RootLayout({
